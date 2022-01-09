3449b13db3ef3ead72074b83e5dabe3cabf36ee8
Charlie George <charliegeorgeanimation@live.co.uk>
2022-01-09 10:40:02 +0000

    Loops

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
 // GENERAL LOOPS
 document.body.innerHTML = `<h1>General Loops</h1>`
 
+// Loops (For when you know how many iterations there are going to be)
+for(let i = 0; i < 10; i++) {
+
+    if(i === 4) {
+        console.log(i + ' is my lucky number.');
+        // Restart loop
+        continue;
+    }
+
+    if(i === 8) {
+        break;
+    }
+
+    console.log('Number ' + i);
+}
+
+let i = 4;
+
+// While Loop (For when you DO NOT know how many iterations there are going to be)
+while(i === 4) {
+    console.log('Hello');
+    break;
+}
